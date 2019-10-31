ea0b5696a41940d7d1d93b7def2d073c76be67b8
Nikolay <kramerdust@yandex.ru>
2019-10-31 19:54:22 +0300

    Graphite added

--- pkg/graphite/client.go
+++ pkg/graphite/client.go
@@ -1,5 +1,33 @@
-package main
+package graphite
 
-func main() {
-	$END$
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+type Client struct {
+	tcpClient net.Conn
+	addr string
+}
+
+func NewGraphiteClient(addr string) (*Client, error) {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		tcpClient: conn,
+		addr:      addr,
+	}, nil
+}
+
+func (gc *Client) AddMetric(path, value string) error {
+	_, err := gc.tcpClient.Write([]byte(fmt.Sprintf("%s %s %d", path, value, time.Now().Unix())))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
